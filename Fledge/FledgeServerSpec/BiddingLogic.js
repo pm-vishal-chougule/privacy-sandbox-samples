d23887a592c8ce71ae51c782eb67f978d9110b76
Vishal Chougule <vishal.chougule@pubmatic.com>
2023-10-25 12:46:21 +0530

    Update ad object by adding render url.

--- Fledge/FledgeServerSpec/BiddingLogic.js
+++ Fledge/FledgeServerSpec/BiddingLogic.js
@@ -17,29 +17,51 @@
 const simulateDelay = (ms) => {
   var start = new Date().getTime();
   var end = start;
-  while(end < start + ms) {
-      end = new Date().getTime();
+  while (end < start + ms) {
+    end = new Date().getTime();
   }
-}
+};
 
 /**
  * Generates a bid of 10 for the shoes CA, and a bid of 5 otherwise
  */
-function generateBid(ad, auction_signals, per_buyer_signals,
-  trusted_bidding_signals, contextual_signals, custom_audience_bidding_signals) {
+function generateBid(
+  ad,
+  auction_signals,
+  per_buyer_signals,
+  trusted_bidding_signals,
+  contextual_signals,
+  custom_audience_bidding_signals
+) {
   var bid = 5;
   if (custom_audience_bidding_signals.name === "shoes") {
-      bid = 10;
+    bid = 10;
   }
   simulateDelay(0);
-  return {'status': 0, 'ad': ad, 'bid': bid };
+  var ad = {
+    render_url: "https://advertiser-1.glitch.me/advertiser/shopping-ad.html",
+    metadata: json_metadata,
+  };
+  return { status: 0, ad: ad, bid: bid };
 }
 
-function reportWin(ad_selection_signals, per_buyer_signals, signals_for_buyer,
- contextual_signals, custom_audience_reporting_signals) {
+function reportWin(
+  ad_selection_signals,
+  per_buyer_signals,
+  signals_for_buyer,
+  contextual_signals,
+  custom_audience_reporting_signals
+) {
   // Add the address of your reporting server here
-  let reporting_address = 'https://reporting.example.com';
+  let reporting_address = "https://reporting.example.com";
   simulateDelay(0);
-  return {'status': 0, 'results': {'reporting_uri':
-         reporting_address + '/reportWin?ca=' + custom_audience_reporting_signals.name} };
+  return {
+    status: 0,
+    results: {
+      reporting_uri:
+        reporting_address +
+        "/reportWin?ca=" +
+        custom_audience_reporting_signals.name,
+    },
+  };
 }
